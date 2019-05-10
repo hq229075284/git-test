03dee82583286de3365c201af342e89808dced5f
HanQ <hanqing4212@tops001.com>
2019-05-10 15:09:53 +0800

    14

--- script/config.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    // devBranch: 'feature-2359',
-    // mergeBranch: 'feature-2037',
-    devBranch: 'master',
-    mergeBranch: 'merge_branch',
-    remoteRepositoryName: 'origin',
-    bashPath:"C:\\Program Files\\Git\\git-bash.exe"
-}
